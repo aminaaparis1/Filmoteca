9da30a84c2cf87d132db2a6cf1233e068bc1066d
aminaaparis1 <amina.zouane@etu.univ-paris1.fr>
2024-11-28 15:09:34 +0100

    update

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -4,52 +4,44 @@ declare(strict_types=1);
 
 namespace App\Core;
 
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
 class Router
 {
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new FilesystemLoader(__DIR__ . '/../../templates');
+        $this->twig = new Environment($loader);
+    }
     public function route(): void
     {
-        /**
-         * echo '<pre>';
-         * var_dump($_SERVER);
-         * echo '</pre>';die;
-         */
-
-        // Récupère l'URL demandée (sans le domaine et la racine)
         $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-
-        // Découpe l'URI pour obtenir la route et l'action
-        $parts = explode('/', $uri); // Exemple : ['films', 'create']
-
-        $route = $parts[0] ?? null;   // 'films'
-        $action = $parts[1] ?? null; // 'create'
-
-        // Définit les routes et leurs contrôleurs associés
+        $parts = explode('/', $uri); 
+        $route = $parts[0] ?? null;   
+        $action = $parts[1] ?? null;  
         $routes = [
             'films' => 'FilmController',
             'contact' => 'ContactController',
         ];
 
         if (array_key_exists($route, $routes)) {
-            // Crée dynamiquement le contrôleur
             $controllerName = 'App\\Controller\\' . $routes[$route];
-
             if (!class_exists($controllerName)) {
                 echo "Controller '$controllerName' not found";
                 return;
             }
 
             $controller = new $controllerName();
-
-            // Vérifie si la méthode existe dans le contrôleur
             if (method_exists($controller, $action)) {
-                $queryParams = $_GET; // Récupère les paramètres éventuels
-                $controller->$action($queryParams); // Appelle la méthode dynamique correspondant à l'action du contrôleur
-            } else {
-                echo "Action '$action' not found in $controllerName";
+                $queryParams = $_GET; 
+                $controller->$action($queryParams); 
+                echo $this->twig->render('404.html.twig', ['title' => 'Page Not Found']);
             }
         } else {
-            // Page non trouvée
-            echo "404 Not Found";
+            echo $this->twig->render('404.html.twig', ['title' => 'Page Not Found']);
         }
     }
 }
